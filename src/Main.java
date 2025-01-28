e4daa70affb3f82bd8330966d96a9b5931cbe6e0
Antonio Carrasco <carrasco.antonio.233@gmail.com>
2025-01-28 10:23:31 +0100

    nivel1

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,8 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Undo undo = Undo.getInstance();
-        Scanner scanner = new Scanner(System.in);
-        String option;
-
-        while (true) {
-            System.out.println("Tria una opció: (1) Afegir comanda, (2) Eliminar última comanda, (3) Llistar comandes, (4) Sortir");
-            option = scanner.nextLine();
-
-            switch (option) {
-                case "1":
-                    System.out.print("Introdueix la comanda: ");
-                    String command = scanner.nextLine();
-                    undo.addCommand(command);
-                    break;
-                case "2":
-                    undo.removeLastCommand();
-                    break;
-                case "3":
-                    undo.listCommands();
-                    break;
-                case "4":
-                    System.out.println("Sortint...");
-                    scanner.close();
-                    return;
-                default:
-                    System.out.println("Opció invàlida. Torna a intentar-ho.");
-            }
-        }
+        Manager manager =new Manager();
+        manager.run();
     }
 }
